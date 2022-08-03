943094086f04e175d6bad8f207529fabc4775c8b
Aiden Fine <fineaiden@gmail.com>
2022-08-03 16:19:20 -0400

    added dark mode

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,5 +1,22 @@
-function MeetupItem() {
-  return <div>Meetup Item</div>;
+import classes from "./MeetupItem.module.css";
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+        <p>{props.time}</p>
+        <p>{props.date}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>Add to Favorites</button>
+      </div>
+    </li>
+  );
 }
 
 export default MeetupItem;
---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,22 @@
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetups) => (
+        <MeetupItem
+          key={meetups.id}
+          id={meetups.id}
+          image={meetups.image}
+          title={meetups.title}
+          address={meetups.address}
+          description={meetups.description}
+          time={meetups.time}
+          date={meetups.date}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default MeetupList;
