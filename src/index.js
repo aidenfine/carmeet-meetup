492231fd74ff20a83febabe04ce704dc3f704fa9
Aiden Fine <fineaiden@gmail.com>
2022-08-24 13:20:34 -0400

    Fixed Bugs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { FavoritesContextProvider } from "./store/favorites-context";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
+  <FavoritesContextProvider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </FavoritesContextProvider>,
+  
   document.getElementById("root")
 );
